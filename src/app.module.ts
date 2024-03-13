86ed09de69f67d6a2c7adea1ef0d9c237cf0f880
ibrahim <ibrahim@addissoftware.com>
2024-03-13 11:45:31 +0300

    refactor: use config for envs.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,19 +4,26 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TodoModule } from './todo/todo.module';
 import { TaskModule } from './task/task.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { join } from 'path';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'new_password',
-      database: 'postgres',
-      // entities: []join(),
-      autoLoadEntities: true,
-      synchronize: true,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [join(process.cwd(), 'dist/**/*.entity{.ts,.js}')],
+        synchronize: true,
+        ssl: { rejectUnauthorized: false },
+      }),
     }),
     TodoModule,
     TaskModule,
